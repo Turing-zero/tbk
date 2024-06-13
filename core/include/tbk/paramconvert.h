196ee52b4bec8194ca18c2b5625b128e8357fa72
mark <mark_zjunlict@zju.edu.cn>
2024-06-13 15:00:17 +0800

    [Fix Bug] fix crash in paramconvert failed

--- core/include/tbk/paramconvert.h
+++ core/include/tbk/paramconvert.h
@@ -1,6 +1,8 @@
 #ifndef __TBK_PARAMCONVERT_H__
 #define __TBK_PARAMCONVERT_H__
 #include <string>
+#include <stdexcept>
+#include "tbk/log.h"
 namespace tbk{
 namespace param{
 template<typename T>
@@ -8,7 +10,14 @@ struct convert{};
 template<>
 struct convert<int>{
     static int to(const std::string& s){
-        return std::stoi(s);
+        try{
+            return std::stoi(s);
+        }catch (std::invalid_argument const& ex){
+            tbk::error("std::stoi - invalid_argument::what(): {}, v: {}\n", ex.what(), s);
+        }catch (std::out_of_range const& ex){
+            tbk::error("std::stoi - out_of_range::what(): {}, v:{}\n", ex.what(), s);
+        }
+        return 0;
     }
     static std::string from(const int& v){
         return std::to_string(v);
@@ -17,7 +26,14 @@ struct convert<int>{
 template<>
 struct convert<double>{
     static double to(const std::string& s){
-        return std::stod(s);
+        try{
+            return std::stod(s);
+        }catch (std::invalid_argument const& ex){
+            tbk::error("std::stod - invalid_argument::what(): {}, v: {}\n", ex.what(), s);
+        }catch (std::out_of_range const& ex){
+            tbk::error("std::stod - out_of_range::what(): {}, v:{}\n", ex.what(), s);
+        }
+        return 0.0;
     }
     static std::string from(const double& v){
         return std::to_string(v);
@@ -35,10 +51,12 @@ struct convert<std::string>{
 template<>
 struct convert<bool>{
     static bool to(const std::string& s){
-        return s=="true";
+        std::string lower(s.length(),0);
+        std::transform(s.begin(), s.end(), lower.begin(), [](unsigned char c){ return std::tolower(c);});
+        return lower=="true";
     }
     static std::string from(const bool& v){
-        return v?"true":"false";
+        return v?"True":"False";
     }
 };
 } // namespace tbk::param
